7c5289004ceb08f0cbac5b960b9c749478d40dde
nww23 <nww23@cornell.edu>
2019-09-28 14:52:41 -0400

    added multiple sensor capability

--- SR04_Single_sensor/SR04_Single_sensor.ino
+++ SR04_Single_sensor/SR04_Single_sensor.ino
@@ -1,28 +1,68 @@
 // SENSOR DEMO
 #include "SRF05.h"
 
-int trigPin_One = ##;
-int echoPin_One = ##;
+int trigPin_One = 2;
+int echoPin_One = 3;
+
+int trigPin_Two = 4;
+int echoPin_Two = 5;
+
+int trigPin_Three = 6;
+int echoPin_Three = 7;
+
+int trigPin_Four = 8;
+int echoPin_Four = 9;
+
+int trigPin_Five = 10;
+int echoPin_Five = 11;
 
 // trigPin, echoPin, MaxDist, readInterval
-SRF05 Sensor_One(trigPin_One, echoPin_One, 300, 500);
+SRF05 Sensor_One(trigPin_One, echoPin_One, 600, 300);
+SRF05 Sensor_Two(trigPin_Two, echoPin_Two, 600, 300);
+SRF05 Sensor_Three(trigPin_Three, echoPin_Three, 600, 300);
+SRF05 Sensor_Four(trigPin_Four, echoPin_Four, 600, 100);
+SRF05 Sensor_Five(trigPin_Five, echoPin_Five, 600, 100);
 
 void setup() {
     Serial.begin(9600);
   // If using SR04 enable the following line:
-  Sensor.Unlock = true;
+  Sensor_One.Unlock = true;
+  Sensor_Two.Unlock = true;
 }
 
 void loop() {
-    // Distance read
+    // Distance read for Sensor One
     if ( Sensor_One.Read() > -1 ) {
         // New distance reading!
-        if ( Sensor.Distance == 0 ) {
-            Serial.println("clear");
+        if ( Sensor_One.Distance == 0 ) {
+            Serial.println("clear I");
         } else {
             // -------------
-            Serial.print(Sensor.Distance);
-            Serial.println(" cm");
+            Serial.print(Sensor_One.Distance);
+            Serial.println(" cm I");
         }
     }
-}
+
+    // Distance read for Sensor Three
+    if ( Sensor_Two.Read() > -1 ) {
+        // New distance reading!
+        if ( Sensor_Two.Distance == 0 ) {
+            Serial.println("clear II");
+        } else {
+            // -------------
+            Serial.print(Sensor_Two.Distance);
+            Serial.println(" cm II");
+        }
+    }
+     // Distance read for Sensor Two
+     if ( Sensor_Three.Read() > -1 ) {
+        // New distance reading!
+        if ( Sensor_Three.Distance == 0 ) {
+            Serial.println("clear III");
+        } else {
+            // -------------
+            Serial.print(Sensor_Three.Distance);
+            Serial.println(" cm III");
+        }
+    }
+ }
